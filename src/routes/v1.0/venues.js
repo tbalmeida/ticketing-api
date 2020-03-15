97e209ab44d13b279688106eaa5b22e1b8e5d3c0
Thiago Almeida <tbalmeida@gmail.com>
2020-03-15 11:57:34 -0600

    Venues routes done

--- src/routes/v1.0/venues.js
+++ src/routes/v1.0/venues.js
@@ -3,14 +3,16 @@ const router = require("express").Router();
 module.exports = db => {
   // route to get all the venues
   router.get("/venues", (request, response) => {
-    db.query(
-      `
+    db.query(`
       SELECT id, name, description, capacity, fee
       FROM venues
       ORDER BY name ASC
-     `).then(({ rows: venues }) => {
+     `)
+    .then(({ rows: venues }) => {
       response.json(venues);
-    });
+    })
+    .catch(e => console.error(e.stack))
+    ;
   });
 
   // get an especific venue
@@ -19,9 +21,11 @@ module.exports = db => {
       SELECT id, name, description, capacity, fee
       FROM venues
       WHERE id = $1::integer
-     `, [request.params.id]).then(({ rows: venues }) => {
+     `, [request.params.id])
+    .then(({ rows: venues }) => {
       response.json(venues);
-    });
+    })
+    .catch(e => console.error(e.stack));
   });
 
   // Get events for this venue
@@ -30,27 +34,55 @@ module.exports = db => {
       SELECT *
       FROM events_vw
       WHERE venue_id = $1::integer
-     `, [request.params.id]).then(({ rows: events }) => {
+     `, [request.params.id])
+    .then(({ rows: events }) => {
       response.json(events);
-    });
+    })
+    .catch(e => console.error(e.stack));
   });
 
-  // route to delete a venue.
-  // router.delete("/venues/:id", (request, response) => {
-  //   if (process.env.TEST_ERROR) {
-  //     setTimeout(() => response.status(500).json({}), 1000);
-  //     return;
-  //   }
+  // Create a new venue
+  router.put("/venues/new", (request, response) => {
+    db.query(`
+      INSERT INTO venues (name, description, capacity, fee) VALUES ($1, $2, $3, $4)
+      RETURNING *;
+      `, [ request.params.name, 
+          request.params.description, 
+          request.params.capacity, 
+          request.params.fee ])
+    .then(({ rows: venues }) => {
+      response.json(venues);
+    })
+    .catch(e => console.error(e.stack));
+  });
 
-  //   db.query(`DELETE FROM venues WHERE id = $1::integer`, [
-  //     request.params.id
-  //   ]).then(() => {
-  //     setTimeout(() => {
-  //       response.status(204).json({});
-  //       updateAppointment(Number(request.params.id), null);
-  //     }, 1000);
-  //   });
-  // });
+
+  // Update a venue
+  router.patch("/venues/:id", (request, response) => {
+    db.query(`
+      UPDATE venues 
+      SET name = $2, description = $3, capacity = $4, fee = $5
+      WHERE id = $1 RETURNING *;
+      `, [ request.params.id,
+          request.params.name, 
+          request.params.description, 
+          request.params.capacity, 
+          request.params.fee ])
+    .then(({ rows: venues }) => {
+      response.json(venues);
+    })
+    .catch(e => console.error(e.stack));
+  });
+
+
+  // delete a venue
+  router.delete("/venues/:id", (request, response) => {
+    db.query(`
+      DELETE FROM venues WHERE id = $1;;
+      `, [ request.params.id ])
+    .then(() => response.status(200).send("Venue deleted successfuly"))
+    .catch(e => console.error(e.stack));
+  });
 
   return router;
 };
