e6a6172260eaa65f12ad58dc1992a3290b37ba08
Thiago Almeida <tbalmeida@gmail.com>
2020-03-24 21:07:15 -0600

    Integrating Stripe use by the API

--- src/helper/receipt.js
+++ src/helper/receipt.js
@@ -1,7 +1,8 @@
-const sendMsg = require( "../../helper/emailHelper");
+// const sendMsg = require( "../../helper/emailHelper");
+const db = require("../db");
 
 db.query(`SELECT * FROM orders_vw WHERE user_handle = $1 ORDER BY order_date DESC, event_date DESC;
-`, [request.params.id])
+`, [4])
 .then(({ rows: orders }) => {
   response.json(orders);
 });
--- src/routes/v1.0/orders.js
+++ src/routes/v1.0/orders.js
@@ -4,39 +4,37 @@ const sendMsg = require( "../../helper/emailHelper");
 module.exports = db => {
   // route to get all the orders. Should be protected, only for system
   router.get("/orders", (request, response) => {
-    db.query(
-      `
-      SELECT *
-      FROM orders_vw
-     `)
-     .then(({ rows: orders }) => {
-      response.status(200).json(orders);
-    });
+    db.query(`SELECT * FROM orders_vw`)
+    .then(({ rows: orders }) => {response.status(200).json(orders)})
+    .catch(e => console.error(e.stack));
   });
 
-  router.get("/orders/:id", () => {
-    db.query(`
-    `)
-    .then(() => {});
-  })
+  router.get("/orders/:id", (request, response) => {
+    db.query(`SELECT * FROM order_details_vw WHERE conf_code = $1`, [ request.params.id ])
+    .then(({ rows: orders }) => {response.status(200).json(orders)})
+    .catch(e => console.error(e.stack));
+  });
 
 
-  // route to delete an event
-  // router.delete("/orders/:id", (request, response) => {
-  //   if (process.env.TEST_ERROR) {
-  //     setTimeout(() => response.status(500).json({}), 1000);
-  //     return;
-  //   }
+  router.put("/orders/new", (request, response) => {
+    try {
+    const { amount  } = req.body;
+      //database needs to save the purchase info
+      // Psst. For production-ready applications we recommend not using the
+      // amount directly from the client without verifying it first. This is to
+      // prevent bad actors from changing the total amount on the client before
+      // it gets sent to the server. A good approach is to send the quantity of
+      // a uniquely identifiable product and calculate the total price server-side.
+      // Then, you would only fulfill orders using the quantity you charged for.
 
-  //   db.query(`DELETE FROM orders WHERE id = $1::integer`, [
-  //     request.params.id
-  //   ]).then(() => {
-  //     setTimeout(() => {
-  //       response.status(204).json({});
-  //       updateAppointment(Number(request.params.id), null);
-  //     }, 1000);
-  //   });
-  // });
+      const paymentIntent = await stripe.paymentIntents.create({ amount, currency: "cad" });
+
+      res.status(200).send(paymentIntent.client_secret);
+    } catch (err) {
+      console.log("err", err)
+      res.status(500).json({ statusCode: 500, message: err.message });
+    }
+  });
 
   return router;
 };
