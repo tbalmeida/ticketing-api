a65d7b136c0de5a9cc2dc4baaf57309fcbc9ec47
Thiago Almeida <tbalmeida@gmail.com>
2020-05-26 14:03:28 -0600

    Testing SendGrid

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ const ENV = require("./environment");
 const app = require("./application")(ENV);
 const server = require("http").Server(app);
 
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'tbalmeida@gmail.com',
+  from: 'ticketing4good@gmail.com',
+  subject: 'Testing',
+  text: 'Testing',
+  html: '<h1>HTML version</h1><p>Testing</p>',
+};
+sgMail.send(msg);
+
 server.listen(PORT, () => {
   console.log(`T4G - Listening on port ${PORT} in ${ENV} mode.`);
 });
